0e759e8333f19526f42464c1bcfb3ef5c3eb2b3a
Andres Castro <andrescastro471@gmail.com>
2026-05-19 00:01:32 -0400

    fix cors

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type']
+}));
+
+app.options('*', cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
